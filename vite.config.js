6ab75203a8babf63f8f44375454414a79c2dd164
Silvia Välk <silviavalk@Silvias-MacBook-Pro.local>
2026-08-20 16:14:45 +0100

    Add GitHub Pages config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Screen-Vision-Prototype/',
   server: { port: 5173, host: true, strictPort: false },
 });
